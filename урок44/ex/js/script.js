c926eccfd2bfcc2f1091321701bfa038f6721da3
Assem <170107159@stu.sdu.edu.kz>
2022-02-13 16:15:34 +0500

    lesson44

---
+++ урок44/ex/js/script.js
@@ -0,0 +1,23 @@
+// should be undefined
+var x;
+console.log(x);
+
+
+x=5;
+if (x == undefined) {
+  console.log("x is undefined");
+}
+
+else {
+  console.log("x has been defined");
+}
+
+
+/*
+если (х == неопределено){
+  выводи мне на кансоль ("x is undefined")
+}
+
+если не так{
+  выводи мне на кансоль ("x has been defined")
+}*/
